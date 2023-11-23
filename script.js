583f007c5c4be1cda78f1e673a569d7930e9c6e7
cisceara <cisceara@gmail.com>
2023-11-22 21:27:55 -0300

    Atualizações layout e design

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplayHoverPause: true,
+  responsive: {
+    0:{
+      items:1,
+      nav: false
+    },
+    600:{
+      items:2,
+      nav: false
+    },
+    1000:{
+      items:3,
+      nav: false
+    }
+  }
+});
+
+//Fim javascript 
+// Framework Owl
